069a89824c6134ef40601a03e081910fd9b2d06b
am4513-blip <am4513@protonmail.com>
2025-07-20 08:33:06 -0700

    comment out TOYOTA_ANGLE_STEERING_LIMITS
    TOYOTA_ANGLE_STEERING_LIMITS only used for vehicles that use Lane Trace Assist (LTA) EPS

--- opendbc/safety/modes/toyota.h
+++ opendbc/safety/modes/toyota.h
@@ -164,20 +164,20 @@ static bool toyota_tx_hook(const CANPacket_t *to_send) {
     .has_steer_req_tolerance = true,
   };
 
-  static const AngleSteeringLimits TOYOTA_ANGLE_STEERING_LIMITS = {
-    // LTA angle limits
-    // factor for STEER_TORQUE_SENSOR->STEER_ANGLE and STEERING_LTA->STEER_ANGLE_CMD (1 / 0.0573)
-    .max_angle = 1657,  // EPS only accepts up to 94.9461
-    .angle_deg_to_can = 17.452007,
-    .angle_rate_up_lookup = {
-      {5., 25., 25.},
-      {0.3, 0.15, 0.15}
-    },
-    .angle_rate_down_lookup = {
-      {5., 25., 25.},
-      {0.36, 0.26, 0.26}
-    },
-  };
+  // static const AngleSteeringLimits TOYOTA_ANGLE_STEERING_LIMITS = {
+  //   // LTA angle limits
+  //   // factor for STEER_TORQUE_SENSOR->STEER_ANGLE and STEERING_LTA->STEER_ANGLE_CMD (1 / 0.0573)
+  //   .max_angle = 1657,  // EPS only accepts up to 94.9461
+  //   .angle_deg_to_can = 17.452007,
+  //   .angle_rate_up_lookup = {
+  //     {5., 25., 25.},
+  //     {0.3, 0.15, 0.15}
+  //   },
+  //   .angle_rate_down_lookup = {
+  //     {5., 25., 25.},
+  //     {0.36, 0.26, 0.26}
+  //   },
+  // };
 
   //const int TOYOTA_LTA_MAX_MEAS_TORQUE = 1500;
   //const int TOYOTA_LTA_MAX_DRIVER_TORQUE = 150;
