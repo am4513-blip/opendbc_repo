838a3cb09fe0ca360d60bb0f92e3ffce974e6334
am4513-blip <am4513@protonmail.com>
2025-07-23 06:49:00 -0700

    added args for cruise message

--- opendbc/car/toyota/carcontroller.py
+++ opendbc/car/toyota/carcontroller.py
@@ -295,7 +295,11 @@ class CarController(CarControllerBase):
     # % 20 = 200ms periodic send rate
     if self.frame % 20 == 0:
       can_sends.append(toyotacan.create_ls_dsu_diag_msg(self.packer, 0x02, 0x21, 0x01))
-      can_sends.append(toyotacan.create_ls_pcm_cruise_command(self.packer, 1, 1, 40))
+      
+     # Send CAN message from DSU to clsuter (0x689) 
+    if (self.frame % 100 == 0): #or send_ui):
+      can_sends.append(toyotacan.create_ls_pcm_cruise_command(self.packer, CS.radar_ready, CS.cruise_active, CS.cc_set_speed))
+      self.prev_set_speed = CS.cc_set_speed
 
     # keep radar disabled
     # if self.frame % 20 == 0 and self.CP.flags & ToyotaFlags.DISABLE_RADAR.value:
