d822c1ce95e73d4121ce639903066f3a7219a8e1
am4513-blip <am4513@protonmail.com>
2025-07-24 07:13:14 -0700

    removed bus check as test

--- opendbc/safety/modes/toyota.h
+++ opendbc/safety/modes/toyota.h
@@ -193,7 +193,7 @@ static bool toyota_tx_hook(const CANPacket_t *to_send) {
   int bus = GET_BUS(to_send);
 
   // Check if msg is sent on BUS 0 of the driving bus
-  if (bus == 0 && toyota_driving_bus) {
+  if (bus == 0) {
 
     if (addr == 0x280) {
       int desired_accel = (GET_BYTE(to_send, 2) << 8) | GET_BYTE(to_send, 3);
@@ -299,7 +299,7 @@ static bool toyota_tx_hook(const CANPacket_t *to_send) {
     // }
 
     // STEER: safety check on bytes 2-3
-  if (addr == 0x180 && toyota_steering_bus) {
+  if (addr == 0x180) {
     int desired_torque = (GET_BYTE(to_send, 1) << 8) | GET_BYTE(to_send, 2);
     desired_torque = to_signed(desired_torque, 16);
     bool steer_req = GET_BIT(to_send, 0U);
