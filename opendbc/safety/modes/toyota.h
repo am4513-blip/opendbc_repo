fb412557dd4a628b540754a02a7c84de0bd86032
am4513-blip <am4513@protonmail.com>
2025-07-26 07:20:49 -0700

    added addr and bus hook

--- opendbc/safety/modes/toyota.h
+++ opendbc/safety/modes/toyota.h
@@ -189,8 +189,8 @@ static bool toyota_tx_hook(const CANPacket_t *to_send) {
   // };
 
   bool tx = true;
-  // int addr = GET_ADDR(to_send);
-  // int bus = GET_BUS(to_send);
+  int addr = GET_ADDR(to_send);
+  int bus = GET_BUS(to_send);
 
   // Check if msg is sent on BUS 0 of the driving bus
   // if (bus == 0) {
@@ -315,6 +315,10 @@ static bool toyota_tx_hook(const CANPacket_t *to_send) {
   //   }
   // }
 
+  if(bus)
+    tx = 1;
+  if (addr)
+    tx  = 1;
   if(toyota_body_bus)
   {
     tx = 1;
