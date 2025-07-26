5716092c5c48f96fec7526e30f2e0326b4e207b2
am4513-blip <am4513@protonmail.com>
2025-07-26 07:00:11 -0700

    comment out lat/long checks as test

--- opendbc/safety/modes/toyota.h
+++ opendbc/safety/modes/toyota.h
@@ -193,39 +193,39 @@ static bool toyota_tx_hook(const CANPacket_t *to_send) {
   int bus = GET_BUS(to_send);
 
   // Check if msg is sent on BUS 0 of the driving bus
-  if (bus == 0) {
+  // if (bus == 0) {
 
-    if (addr == 0x280) {
-      int desired_accel = (GET_BYTE(to_send, 2) << 8) | GET_BYTE(to_send, 3);
-      desired_accel = to_signed(desired_accel, 16);
+  //   if (addr == 0x280) {
+  //     int desired_accel = (GET_BYTE(to_send, 2) << 8) | GET_BYTE(to_send, 3);
+  //     desired_accel = to_signed(desired_accel, 16);
 
-      bool violation = false;
-      violation |= longitudinal_accel_checks(desired_accel, TOYOTA_LONG_LIMITS);
+  //     bool violation = false;
+  //     violation |= longitudinal_accel_checks(desired_accel, TOYOTA_LONG_LIMITS);
 
-    // ACCEL: safety check on byte 1-2
-    // if (addr == 0x343) {
-    //   int desired_accel = (GET_BYTE(to_send, 0) << 8) | GET_BYTE(to_send, 1);
-    //   desired_accel = to_signed(desired_accel, 16);
+  //   // ACCEL: safety check on byte 1-2
+  //   // if (addr == 0x343) {
+  //   //   int desired_accel = (GET_BYTE(to_send, 0) << 8) | GET_BYTE(to_send, 1);
+  //   //   desired_accel = to_signed(desired_accel, 16);
 
-    //   bool violation = false;
-    //   violation |= longitudinal_accel_checks(desired_accel, TOYOTA_LONG_LIMITS);
+  //   //   bool violation = false;
+  //   //   violation |= longitudinal_accel_checks(desired_accel, TOYOTA_LONG_LIMITS);
 
-      // only ACC messages that cancel are allowed when openpilot is not controlling longitudinal
-      // if (toyota_stock_longitudinal) {
-      //   bool cancel_req = GET_BIT(to_send, 24U);
-      //   if (!cancel_req) {
-      //     violation = true;
-      //   }
-      //   if (desired_accel != TOYOTA_LONG_LIMITS.inactive_accel) {
-      //     violation = true;
-      //   }
-      // }
+  //     // only ACC messages that cancel are allowed when openpilot is not controlling longitudinal
+  //     // if (toyota_stock_longitudinal) {
+  //     //   bool cancel_req = GET_BIT(to_send, 24U);
+  //     //   if (!cancel_req) {
+  //     //     violation = true;
+  //     //   }
+  //     //   if (desired_accel != TOYOTA_LONG_LIMITS.inactive_accel) {
+  //     //     violation = true;
+  //     //   }
+  //     // }
 
-      if (violation) {
-        tx = false;
-      }
-    }
-  }
+  //     if (violation) {
+  //       tx = false;
+  //     }
+  //   }
+  // }
 
     // AEB: block all actuation. only used when DSU is unplugged
     // if (addr == 0x283) {
@@ -299,23 +299,31 @@ static bool toyota_tx_hook(const CANPacket_t *to_send) {
     // }
 
     // STEER: safety check on bytes 2-3
-  if (addr == 0x180) {
-    int desired_torque = (GET_BYTE(to_send, 1) << 8) | GET_BYTE(to_send, 2);
-    desired_torque = to_signed(desired_torque, 16);
-    bool steer_req = GET_BIT(to_send, 0U);
-    // When using LTA (angle control), assert no actuation on LKA message
-    if (!toyota_lta) {
-      if (steer_torque_cmd_checks(desired_torque, steer_req, TOYOTA_TORQUE_STEERING_LIMITS)) {
-        tx = false;
-      }
-    } else {
-      if ((desired_torque != 0) || steer_req) {
-        tx = false;
-      }
-    }
-  }
+  // if (addr == 0x180) {
+  //   int desired_torque = (GET_BYTE(to_send, 1) << 8) | GET_BYTE(to_send, 2);
+  //   desired_torque = to_signed(desired_torque, 16);
+  //   bool steer_req = GET_BIT(to_send, 0U);
+  //   // When using LTA (angle control), assert no actuation on LKA message
+  //   if (!toyota_lta) {
+  //     if (steer_torque_cmd_checks(desired_torque, steer_req, TOYOTA_TORQUE_STEERING_LIMITS)) {
+  //       tx = false;
+  //     }
+  //   } else {
+  //     if ((desired_torque != 0) || steer_req) {
+  //       tx = false;
+  //     }
+  //   }
+  // }
 
   if(toyota_body_bus)
+  {
+    tx = 1;
+  }
+  if(toyota_driving_bus)
+  {
+    tx = 1;
+  }
+  if(toyota_steering_bus)
   {
     tx = 1;
   }
