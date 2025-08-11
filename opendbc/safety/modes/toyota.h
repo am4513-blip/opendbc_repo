9b6c494e05daea5d6775b098784e77df0bda8009
am4513-blip <am4513@protonmail.com>
2025-08-11 12:58:07 -0700

    set ignore_quality_flag to false

--- opendbc/safety/modes/toyota.h
+++ opendbc/safety/modes/toyota.h
@@ -35,7 +35,7 @@
   {0x343, 0, 8, .check_relay = true},  \
 
 #define TOYOTA_COMMON_RX_CHECKS(lta)                                                                                                       \
-  {.msg = {{0x260, 0, 8, .ignore_counter = true, .ignore_quality_flag=true, /*!(lta),*/ .frequency = 50U}, { 0 }, { 0 }}},                           \
+  {.msg = {{0x260, 0, 8, .ignore_counter = true, .ignore_quality_flag=false, /*!(lta),*/ .frequency = 50U}, { 0 }, { 0 }}},                 \
   {.msg = {{ 0xB0, 1, 8, .ignore_checksum = true, .ignore_counter = true, .ignore_quality_flag = true, .frequency = 83U}, { 0 }, { 0 }}},  \
   {.msg = {{ 0xB2, 1, 8, .ignore_checksum = true, .ignore_counter = true, .ignore_quality_flag = true, .frequency = 83U}, { 0 }, { 0 }}},  \
   {.msg = {{0x2C1, 1, 8, .ignore_checksum = true, .ignore_counter = true, .frequency = 31U}, { 0 }, { 0 }}},                               \
