458a03ce9d489ded8b7b7a406f5566d820b0a810
am4513-blip <am4513@protonmail.com>
2025-08-11 08:41:19 -0700

    removed fcw_command message

--- opendbc/car/toyota/carcontroller.py
+++ opendbc/car/toyota/carcontroller.py
@@ -268,8 +268,8 @@ class CarController(CarControllerBase):
       #                                                hud_control.rightLaneVisible, hud_control.leftLaneDepart,
       #                                                hud_control.rightLaneDepart, CC.enabled, CS.lkas_hud))
 
-      if (self.frame % 100 == 0 or send_ui) and (self.CP.enableDsu or self.CP.flags & ToyotaFlags.DISABLE_RADAR.value):
-        can_sends.append(toyotacan.create_fcw_command(self.packer, fcw_alert))
+      # if (self.frame % 100 == 0 or send_ui) and (self.CP.enableDsu or self.CP.flags & ToyotaFlags.DISABLE_RADAR.value):
+      #   can_sends.append(toyotacan.create_fcw_command(self.packer, fcw_alert))
 
     # *** static msgs ***
     if self.CP.enableDsu:
