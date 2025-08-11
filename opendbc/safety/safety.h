f3406e9ed55c45f85427598fa61e376b716a91b8
am4513-blip <am4513@protonmail.com>
2025-08-11 13:39:30 -0700

    comment out quality_flag check as test

--- opendbc/safety/safety.h
+++ opendbc/safety/safety.h
@@ -96,7 +96,7 @@ static void stock_ecu_check(bool stock_ecu_detected);
 static bool is_msg_valid(RxCheck addr_list[], int index) {
   bool valid = true;
   if (index != -1) {
-    if (/*!addr_list[index].status.valid_checksum || */!addr_list[index].status.valid_quality_flag || (addr_list[index].status.wrong_counters >= MAX_WRONG_COUNTERS)) {
+    if (!addr_list[index].status.valid_checksum || /*!addr_list[index].status.valid_quality_flag ||*/ (addr_list[index].status.wrong_counters >= MAX_WRONG_COUNTERS)) {
       valid = false;
       controls_allowed = false;
     }
