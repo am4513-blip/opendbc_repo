b971faeda0a1bb7379ef12e0c517719e3c0b2b40
am4513-blip <am4513@protonmail.com>
2025-08-18 08:23:49 -0700

    changed 0x2E4 to 0x180 in COMMON_TX_MSGS

--- opendbc/safety/modes/toyota.h
+++ opendbc/safety/modes/toyota.h
@@ -8,7 +8,7 @@
 
 #define TOYOTA_COMMON_TX_MSGS \
   TOYOTA_BASE_TX_MSGS \
-  {0x2E4, 0, 5, .check_relay = true}, \
+  {0x180, 0, 5, .check_relay = true}, \
   {0x280, 0, 8, .check_relay = false},  /* ACC cancel cmd */  \
 
 #define TOYOTA_COMMON_SECOC_TX_MSGS \
