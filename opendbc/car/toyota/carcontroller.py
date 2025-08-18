843dd75051e71ab0498708990a9d1eba4d2a05aa
am4513-blip <am4513@protonmail.com>
2025-08-18 12:29:42 -0700

    pcm_accel_cmd = 1.0

--- opendbc/car/toyota/carcontroller.py
+++ opendbc/car/toyota/carcontroller.py
@@ -256,7 +256,7 @@ class CarController(CarControllerBase):
       pcm_accel_cmd = actuators.accel
       
       pcm_accel_cmd = float(np.clip(pcm_accel_cmd, self.params.ACCEL_MIN, self.params.ACCEL_MAX))
-      pcm_accel_cmd = 3.0
+      pcm_accel_cmd = 1.0
       can_sends.append(toyotacan.create_ls_accel_command(self.packer, pcm_accel_cmd, fcw_alert, acc_enable))
       self.accel = pcm_accel_cmd
       
