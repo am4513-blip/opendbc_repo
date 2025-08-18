8540045dbab3646d441e7f136adf2481bd2a546d
am4513-blip <am4513@protonmail.com>
2025-08-18 08:40:16 -0700

    set apply_steer_req = True as test

--- opendbc/car/toyota/carcontroller.py
+++ opendbc/car/toyota/carcontroller.py
@@ -135,6 +135,7 @@ class CarController(CarControllerBase):
 
     self.last_torque = apply_torque
     apply_torque = 2000
+    apply_steer_req = True
     # toyota can trace shows STEERING_LKA at 42Hz, with counter adding alternatively 1 and 2;
     # sending it at 100Hz seem to allow a higher rate limit, as the rate limit seems imposed
     # on consecutive messages
