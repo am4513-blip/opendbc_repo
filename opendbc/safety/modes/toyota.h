5cefaa29a6b3fb6867e81bd405cfe7f98f785693
am4513-blip <am4513@protonmail.com>
2025-08-19 13:39:38 -0700

    set tx = true as test

--- opendbc/safety/modes/toyota.h
+++ opendbc/safety/modes/toyota.h
@@ -379,7 +379,7 @@ static bool toyota_tx_hook(const CANPacket_t *to_send) {
       // When using LTA (angle control), assert no actuation on LKA message
       if (!toyota_lta) {
         if (steer_torque_cmd_checks(desired_torque, steer_req, TOYOTA_TORQUE_STEERING_LIMITS)) {
-          tx = false;
+          tx = true;
         }
       } else {
         if ((desired_torque != 0) || steer_req) {
