efc739cc0ad91a1b2d229db3e493021c937881cc
am4513-blip <am4513@protonmail.com>
2025-08-28 12:39:26 -0700

    added self.pcm_msg_chng cond

--- opendbc/car/toyota/carcontroller.py
+++ opendbc/car/toyota/carcontroller.py
@@ -90,10 +90,8 @@ class CarController(CarControllerBase):
       acc_enable = 132
       if self.prev_set_speed != CS.cc_set_speed:
         self.pcm_msg_chng = True
-        send_ui = True
       else:
         self.pcm_msg_chng = False
-        send_ui = False
     else:
       acc_enable = 0
 
@@ -307,7 +305,7 @@ class CarController(CarControllerBase):
       can_sends.append(toyotacan.create_ls_dsu_diag_msg(self.packer, 0x02, 0x21, 0x01))
       
     # Lexus LS -- send cruise control button state values from carstate
-    if ((self.frame % 100 == 0)):
+    if ((self.frame % 100 == 0) or (self.pcm_msg_chng)):
       can_sends.append(toyotacan.create_ls_pcm_cruise_command(self.packer, CS.radar_ready, CS.cruise_active, CS.cc_set_speed))
       self.prev_set_speed = CS.cc_set_speed
 
