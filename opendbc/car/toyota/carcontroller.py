96584ac97fa02a28863686755a49839def2a43c9
am4513-blip <am4513@protonmail.com>
2025-08-28 11:50:52 -0700

    added print statement

--- opendbc/car/toyota/carcontroller.py
+++ opendbc/car/toyota/carcontroller.py
@@ -274,8 +274,10 @@ class CarController(CarControllerBase):
         
         
     # *** hud ui ***
-    # if self.pcm_msg_chng: ######### LEXUS_LS #########
-    #   send_ui = True
+    if self.pcm_msg_chng: ######### LEXUS_LS #########
+      send_ui = True
+      print(self.pcm_msg_chng)
+      print(send_ui)
     # if self.CP.carFingerprint != CAR.TOYOTA_PRIUS_V:
     #   # ui mesg is at 1Hz but we send asap if:
     #   # - there is something to display
@@ -303,7 +305,7 @@ class CarController(CarControllerBase):
         can_sends.append(toyotacan.create_ls_dsu_diag_msg(self.packer, 0x02, 0x21, 0x01))
       
       # Lexus LS -- send cruise control button state values from carstate
-      if ((self.frame % 100 == 0)):
+      if ((self.frame % 100 == 0) or (send_ui)):
         can_sends.append(toyotacan.create_ls_pcm_cruise_command(self.packer, CS.radar_ready, CS.cruise_active, CS.cc_set_speed))
         self.prev_set_speed = CS.cc_set_speed
 
