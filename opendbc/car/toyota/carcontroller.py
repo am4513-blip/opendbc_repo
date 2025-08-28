a8132cce6c49b88b29c6d3995cadad33b033a43f
am4513-blip <am4513@protonmail.com>
2025-08-28 12:31:42 -0700

    moved if statments indent

--- opendbc/car/toyota/carcontroller.py
+++ opendbc/car/toyota/carcontroller.py
@@ -302,14 +302,14 @@ class CarController(CarControllerBase):
       #   can_sends.append(toyotacan.create_fcw_command(self.packer, fcw_alert))
       
       
-      #Lexus LS -- Send diagnostic request for cruise control stalk button status every 200ms
-      if (self.frame % 20 == 0):
-        can_sends.append(toyotacan.create_ls_dsu_diag_msg(self.packer, 0x02, 0x21, 0x01))
+    #Lexus LS -- Send diagnostic request for cruise control stalk button status every 200ms
+    if (self.frame % 20 == 0):
+      can_sends.append(toyotacan.create_ls_dsu_diag_msg(self.packer, 0x02, 0x21, 0x01))
       
-      # Lexus LS -- send cruise control button state values from carstate
-      if ((self.frame % 100 == 0) or (send_ui)):
-        can_sends.append(toyotacan.create_ls_pcm_cruise_command(self.packer, CS.radar_ready, CS.cruise_active, CS.cc_set_speed))
-        self.prev_set_speed = CS.cc_set_speed
+    # Lexus LS -- send cruise control button state values from carstate
+    if ((self.frame % 100 == 0) or (send_ui)):
+      can_sends.append(toyotacan.create_ls_pcm_cruise_command(self.packer, CS.radar_ready, CS.cruise_active, CS.cc_set_speed))
+      self.prev_set_speed = CS.cc_set_speed
 
     # *** static msgs ***
     # if self.CP.enableDsu:
