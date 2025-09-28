df6ac28f20e3eff57d4b522623cba99d58c812bc
am4513-blip <am4513@protonmail.com>
2025-09-28 09:40:27 -0700

    chnged delta down to 15 as test

--- opendbc/car/toyota/values.py
+++ opendbc/car/toyota/values.py
@@ -43,10 +43,10 @@ class CarControllerParams:
 
     if CP.lateralTuning.which() == 'torque':
       self.STEER_DELTA_UP = 10       # 1.0s time to peak torque
-      self.STEER_DELTA_DOWN = 10     # always lower than 45 otherwise the Rav4 faults (Prius seems ok with 50)
+      self.STEER_DELTA_DOWN = 15     # always lower than 45 otherwise the Rav4 faults (Prius seems ok with 50)
     else:
       self.STEER_DELTA_UP = 10       # 1.5s time to peak torque
-      self.STEER_DELTA_DOWN = 10     # always lower than 45 otherwise the Rav4 faults (Prius seems ok with 50)
+      self.STEER_DELTA_DOWN = 15     # always lower than 45 otherwise the Rav4 faults (Prius seems ok with 50)
 
 
 class ToyotaSafetyFlags(IntFlag):
