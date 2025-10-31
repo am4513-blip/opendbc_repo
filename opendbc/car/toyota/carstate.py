3f1d3f8819fcab1a8afc52432dc559bb7fb0af35
am4513-blip <am4513@protonmail.com>
2025-10-31 09:43:43 -0700

    remove mask

--- opendbc/car/toyota/carstate.py
+++ opendbc/car/toyota/carstate.py
@@ -255,7 +255,7 @@ class CarState(CarStateBase):
        (self.CP.carFingerprint in TSS2_CAR and self.acc_type == 1):
       if self.CP.openpilotLongitudinalControl:
         #ret.accFaulted = ret.accFaulted or cp.vl["PCM_CRUISE_2"]["LOW_SPEED_LOCKOUT"] == 2
-        ret.accFaulted= ((cp_drv.vl["VSC_DATA7"]["BRK_ERR_FLGS"]) & 251) != 0  ############### LEXUS_LS ########################
+        ret.accFaulted = ((cp_drv.vl["VSC_DATA7"]["BRK_ERR_FLGS"])) != 0  ############### LEXUS_LS ########################
 
     self.pcm_acc_status = 8 #cp.vl["PCM_CRUISE"]["CRUISE_STATE"]   ############### LEXUS_LS ########################
     if self.CP.carFingerprint not in (NO_STOP_TIMER_CAR - TSS2_CAR):
