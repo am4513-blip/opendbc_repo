10dcc99e70d5878d0a76575d0c38419039a59778
am4513-blip <am4513@protonmail.com>
2025-10-31 17:35:55 -0700

    steering_angle_deadzone_deg back to 0

--- opendbc/car/toyota/interface.py
+++ opendbc/car/toyota/interface.py
@@ -48,7 +48,7 @@ class CarInterface(CarInterfaceBase):
       ret.steerActuatorDelay = 0.18
       ret.steerLimitTimer = 0.8
     else:
-      CarInterfaceBase.configure_torque_tune(candidate, ret.lateralTuning, steering_angle_deadzone_deg = 0.6)
+      CarInterfaceBase.configure_torque_tune(candidate, ret.lateralTuning, steering_angle_deadzone_deg = 0.0)
       ret.steerActuatorDelay = 0.12  # Default delay, Prius has larger delay
       ret.steerLimitTimer = 0.4
 
